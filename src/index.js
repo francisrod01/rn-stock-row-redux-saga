238de28fa019c648bedb30f465de0ebedee9f228
Francis Rodrigues <francis.rod01@outlook.com>
2019-03-13 01:32:52 -0300

    ENH: Refactor index file and import redux

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import "./config/reactotron";
+import store from "./store";
+
+import RepositoryList from "./components/RepositoryList";
+
+export const App = () => (
+  <Provider store={store}>
+    <RepositoryList />
+  </Provider>
+);
+
+export default App;

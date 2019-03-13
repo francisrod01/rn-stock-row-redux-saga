9afbc34a04f5339b6b5b187fcc3f9f504c2573aa
Francis Rodrigues <francis.rod01@outlook.com>
2019-03-13 00:52:17 -0300

    Implementing store from redux

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
+
+const middlewares = [];
+
+const sagaMiddleware = createSagaMiddleware();
+
+middlewares.push(sagaMiddleware);
+
+const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
+
+const store = createAppropriateStore(
+  rootReducer,
+  applyMiddleware(...middlewares)
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;

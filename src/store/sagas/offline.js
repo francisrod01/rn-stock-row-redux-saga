2749a29a82ae0e54aeb6a82733a36cfd192a73c7
Francis Rodrigues <francis.rod01@outlook.com>
2019-03-13 19:54:56 -0300

    Implemented a offline saga

---
+++ src/store/sagas/offline.js
@@ -0,0 +1,28 @@
+import { put, take } from "redux-saga/effects";
+import { eventChannel } from "redux-saga";
+import { NetInfo } from "react-native";
+import { OFFLINE, ONLINE } from "redux-offline-queue";
+
+export function* startWatchingNetworkConnectivity() {
+  const channel = eventChannel(emitter => {
+    NetInfo.isConnected.addEventListener("connectionChange", emitter);
+
+    return () => NetInfo.isConnected.removeEventListener("connectionChange", emitter);
+  });
+
+  try {
+    while (true) {
+      const isConnected = yield take(channel);
+
+      if (isConnected) {
+        yield put({ type: ONLINE });
+      }
+      else {
+        yield put({ type: OFFLINE });
+      }
+    }
+  }
+  finally {
+    channel.close();
+  }
+}

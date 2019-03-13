be0050bdd193e0087b141da1ea3fb6e52c5f4c84
Francis Rodrigues <francis.rod01@outlook.com>
2019-03-13 19:56:09 -0300

    Imported offline saga into rootSaga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,10 +1,13 @@
-import { all, takeEvery } from "redux-saga/effects";
+import { all, spawn, takeEvery } from "redux-saga/effects";
 
 import { RepositoriesTypes } from "../reducers/repositories";
 import { addRepository } from "./repositories";
 
+import { startWatchingNetworkConnectivity } from "./offline";
+
 export default function* rootSaga() {
   yield all([
+    spawn(startWatchingNetworkConnectivity),
     takeEvery(RepositoriesTypes.ADD_REPOSITORY_REQUEST, addRepository)
   ]);
 }
